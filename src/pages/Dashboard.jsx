ae3905fe59e42ba307af6ce5a0cde86670090406
Santhosh kumar S <116993062+Santhosh1276@users.noreply.github.com>
2025-05-28 22:17:40 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 import React from "react";
 import ShowCountsDashboard from "../components/ShowCountsDashboard";
-import InterviewStatsDashboard from "../components/InterviewStatsDashboard.JSX";
+import InterviewStatsDashboard from "../components/InterviewStatsDashboard";
 
 const Dashboard = () => {
   return (
